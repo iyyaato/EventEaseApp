a8afc30e2dd123d14b5e3b907eb5df0d4e676f80
Isaac Tharcisse Yaato <isaacyaato97@gmail.com>
2025-09-21 14:38:16 -0400

    Update email.js

--- wwwroot/email.js
+++ wwwroot/email.js
@@ -1,12 +1,1 @@
-(function() {
-    emailjs.init("ZOYvRrE8ONR6Xu7TA"); // Your Public Key
-})();
-
-function sendMail(form) {
-    emailjs.sendForm("service_4acey1c", "template_9h9rnh9", form)
-        .then(function () {
-            document.getElementById("form-status").innerText = "✅ Message sent successfully!";
-        }, function (error) {
-            document.getElementById("form-status").innerText = "❌ Failed to send: " + error.text;
-        });
-}
+Delete unused email.js file
